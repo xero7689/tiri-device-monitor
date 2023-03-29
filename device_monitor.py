1e013a154bdea81b180bae4447af975356770c93
xero7689 <volleyp7689@gmail.com>
2023-03-29 23:34:53 +0800

    Update: Settings of device monitor

--- device_monitor.py
+++ device_monitor.py
@@ -2,12 +2,14 @@ import os
 import random
 import time
 
-data_dir = "./data"
+import settings
+
+data_dir = settings.DATA_PATH
 
 # File_1_SensorA_20230318
-file_header = "File"
-file_serial_num = "1"
-file_device_name = "SensorA"
+file_header = settings.FILE_HEADER
+file_serial_num = settings.FILE_SERIAL_NUM
+file_device_name = settings.FILE_DEVICE_NAME
 
 file_name = file_header + "_" + file_serial_num + "_" + file_device_name
 file_name = file_name + "_" + time.strftime("%Y%m%d") + ".txt"
@@ -19,12 +21,15 @@ if not os.path.exists(file_path):
 
 while True:
     timestamp = time.strftime("%T")
-    temperature = random.uniform(25, 32)
-    humidity = random.random() * 100
+    temperature = round(random.uniform(25, 32), 2)
+    humidity = round(random.random() * 100, 2)
     resistance = random.randint(1000, 1200)
     concentration = random.randint(1000, 2000)
 
     with open(file_path, "a", encoding="utf-8") as f:
+        s = f"{timestamp},{temperature},{humidity},{resistance},{concentration}"
+        print(f'[+] {s}')
+
         f.write(f"{timestamp}，溫度：{temperature}，單位攝氏。C，濕度：{humidity}，%，原始電阻：{resistance}，轉換濃度：{concentration}\n")
     
     time.sleep(1)
